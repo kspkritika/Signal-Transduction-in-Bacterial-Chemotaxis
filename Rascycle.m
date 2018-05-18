d7413729205b5fb5a34a0abbc31755e2ec9ddde3
kspkritika <35979747+kspkritika@users.noreply.github.com>
2018-05-17 21:20:14 -0400

    changed

--- Rascycle.m
+++ Rascycle.m
@@ -1,5 +1,1 @@
 function [t, y] = Rascycle(cAMP,Tend,y0,S,standard)
-%if nargin == 2, 
-%S = 'b'; standard = 1; end % setting defaults
-%if nargin == 3, S = 'b'; standard = 1; end
-%if nargin == 4, standard = 1; end
